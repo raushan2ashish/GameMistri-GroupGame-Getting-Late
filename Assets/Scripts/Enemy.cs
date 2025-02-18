648912d29056165a44548e29071c3a74192fcbf7
Korhan <167242717+El-Kokoshi@users.noreply.github.com>
2025-02-18 16:20:25 -0500

    Dog enemy Update
    Dog enemy idle added
    Dog enemy script updated for fixing the dog on y axis
    Dog Territories updated on all levels

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,41 +6,56 @@ public class Enemy : MonoBehaviour
 {
     
     public int health;
+    public float initPosY;
+    public float updatePosY;
     public bool isActive;
     public bool isIdle = true;
     public bool movingRight = false;
     public bool facingRight = false;
     public Rigidbody2D rb;
+    public Animator animator;
+
     
 
     // Start is called before the first frame update
     public void Start()
     {
         GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         health = 2;
         isActive = false;
+        initPosY = transform.position.y;
     }
 
     // Update is called once per frame
     public void Update()
     {
+        updatePosY = transform.position.y;
         if(rb.velocity.x < 0)
         {
             facingRight = false;
             isIdle = false;
             AnimationFlipper();
+            animator.SetBool("isMoving", true);
         }
         else if(rb.velocity.x > 0)
         {
             facingRight = true;
             isIdle = false;
             AnimationFlipper();
+            animator.SetBool("isMoving", true);
         }
         else if(rb.velocity.x == 0)
         {
             isIdle = true;
+            animator.SetBool("isMoving", false);
         }
 
+        if(updatePosY != initPosY)
+        {
+            updatePosY = initPosY;
+            transform.position = new Vector2(transform.position.x, updatePosY);
+        }
         
     }
 
