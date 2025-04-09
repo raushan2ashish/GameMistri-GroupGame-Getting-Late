55a6cca7b4b358d2d70bca8bded914351c4b8fd3
raushan2ashish <raushan22@live.com>
2025-04-09 17:24:02 -0400

    added Demo Gameplay,

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+    
+    public void LoadSceneByName(string sceneName)
+    {
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning($"Scene '{sceneName}' cannot be loaded. Make sure it's added to the Build Settings.");
+        }
+    }
+}
